d3b15a456ba17298cf8f5431523fbb00b2d6abd7
abacsrob <abacsrob@gmail.com>
2018-07-28 20:51:48 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/recipedemo/recipedemo/RecipedemoApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.recipedemo.recipedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipedemoApplication.class, args);
+	}
+}
